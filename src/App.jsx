f52f9eaa33e12fbd2b074da5e2042d0aaee46f2d
AmoushRye <amoskhams1234@gmail.com>
2023-11-13 19:57:07 +0530

    api fetching done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Header from "./components/Header";
+import Coins from "./components/Coins"
+import Exchanges from "./components/Exchanges"
+import Home from "./components/Home"
+import CoinDetails from "./components/CoinDetails"
 
 function App() {
   return (
-    <div>
-      Starting Soon......
-      <h1>wait for a while</h1>
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element= {<Home/>}/>
+        <Route path='/coins' element= {<Coins/>}/>
+        <Route path='/exchanges' element= {<Exchanges/>}/>
+        <Route path='/coindetails/:id' element= {<CoinDetails/>}/>
+      </Routes>
+    </Router>
   )
 }
 
